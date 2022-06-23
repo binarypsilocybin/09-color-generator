ee434eccea68a5e4b7b108ba6b5778ffd0bb0297
binarypsilocybin <rogeriosvaldo@gmail.com>
2022-06-23 11:17:11 +0100

    2 files modified

--- src/App.js
+++ src/App.js
@@ -37,7 +37,15 @@ function App() {
       <section className="colors">
         {list.map((color, index) => {
           console.log(color);
-          return <SingleColor key={index} {...color} index={index} />;
+
+          return (
+            <SingleColor
+              key={index}
+              {...color}
+              index={index}
+              hexColor={color.hex}
+            />
+          );
         })}
       </section>
     </>
--- src/SingleColor.js
+++ src/SingleColor.js
@@ -1,11 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import rgbToHex from './utils';
 
-const SingleColor = ({ rgb, weight, index }) => {
+const SingleColor = ({ rgb, weight, index, hexColor }) => {
   const [alert, setAlert] = useState(false);
   const bcg = rgb.join(',');
   console.log(bcg);
-  return <h4>single color</h4>;
+  const hex = rgbToHex(..rgb);
+  return (
+    <article className={`color`} style={{ backgroundColor: `rgb(${bcg})` }}>
+      <p className="percent-value">{weight}%</p>
+      <p className="color-value">{hexColor}</p>
+    </article>
+  );
 };
 
 export default SingleColor;
