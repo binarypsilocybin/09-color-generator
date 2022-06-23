5575e717004cce8ff45e25999f779052ec785835
binarypsilocybin <rogeriosvaldo@gmail.com>
2022-06-23 10:31:57 +0100

    2 files modified

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
     e.preventDefault();
     try {
       let colors = new Values(color).all(10);
+      setList(colors);
     } catch (error) {
       setError(true);
       console.log(error);
@@ -34,7 +35,10 @@ function App() {
         </form>
       </section>
       <section className="colors">
-        <h4>list goes here</h4>
+        {list.map((color, index) => {
+          console.log(color);
+          return <SingleColor key={index} {...color} index={index} />;
+        })}
       </section>
     </>
   );
